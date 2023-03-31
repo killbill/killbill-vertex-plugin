bb976229d6723458e9d21a82afb459c41f752455
Konrad.Lubianka <konrad.lubianka@eu.equinix.com>
2023-03-31 10:48:34 +0200

    Call vertex HealthCheck web service

---
+++ src/main/java/org/killbill/billing/plugin/vertex/health/VertexHealthcheck.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020-2023 Equinix, Inc
+ * Copyright 2020-2023 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.killbill.billing.plugin.vertex.health;
+
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import org.killbill.billing.osgi.api.Healthcheck;
+import org.killbill.billing.plugin.vertex.gen.health.HealthCheckService;
+import org.killbill.billing.tenant.api.Tenant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vertexinc.ws.healthcheck.generated.LoginType;
+import com.vertexinc.ws.healthcheck.generated.PerformHealthCheckRequest;
+import com.vertexinc.ws.healthcheck.generated.PerformHealthCheckResponseType;
+
+public class VertexHealthcheck implements Healthcheck {
+
+    private static final Logger logger = LoggerFactory.getLogger(VertexHealthcheck.class);
+
+    private final HealthCheckApiConfigurationHandler healthCheckApiConfigurationHandler;
+
+    public VertexHealthcheck(final HealthCheckApiConfigurationHandler healthCheckApiConfigurationHandler) {
+        this.healthCheckApiConfigurationHandler = healthCheckApiConfigurationHandler;
+    }
+
+    @Override
+    public HealthStatus getHealthStatus(@Nullable final Tenant tenant, @Nullable final Map properties) {
+        if (tenant == null) {
+            // The plugin is running
+            return HealthStatus.healthy("Vertex OK (unauthenticated)");
+        } else {
+            // Specifying the tenant lets you also validate the tenant configuration
+            final HealthCheckService healthCheckService = healthCheckApiConfigurationHandler.getConfigurable(tenant.getId());
+            try {
+                PerformHealthCheckRequest healthCheckRequest = new PerformHealthCheckRequest();
+                LoginType login = new LoginType();
+                login.setUserName(tenant.getApiKey());//fixme is it OK to set username/password
+                login.setPassword(tenant.getApiSecret());
+                healthCheckRequest.setLogin(login);
+                PerformHealthCheckResponseType healthCheckResponse = healthCheckService.getHealthCheckPort().performHealthCheck(healthCheckRequest);
+                boolean healthy = "OK".equals(healthCheckResponse.getCalcEngine());
+                return healthy ? HealthStatus.healthy("Vertex CalcEngine status: OK") : HealthStatus.unHealthy("Vertex CalcEngine status: " + healthCheckResponse.getCalcEngine());
+            } catch (Exception e) {
+                return HealthStatus.unHealthy(e.getMessage());
+            }
+        }
+    }
+}
