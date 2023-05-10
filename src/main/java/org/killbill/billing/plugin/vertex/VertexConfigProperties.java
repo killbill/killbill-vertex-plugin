ed92e62366564f965ee4538b9f1001831e2332c6
Vlas Khilkevich <93208596+vlaskhilkevich@users.noreply.github.com>
2023-05-10 20:17:25 +0200

    Remove VERTEX_SKIP constant from vertex config properties

--- src/main/java/org/killbill/billing/plugin/vertex/VertexConfigProperties.java
+++ src/main/java/org/killbill/billing/plugin/vertex/VertexConfigProperties.java
@@ -20,11 +20,11 @@ package org.killbill.billing.plugin.vertex;
 public class VertexConfigProperties {
 
     public static final String PROPERTY_PREFIX = "org.killbill.billing.plugin.vertex.";
-    public static final String VERTEX_SKIP = "VERTEX_SKIP";
+
     public static final String VERTEX_OSERIES_URL_PROPERTY = PROPERTY_PREFIX + "url";
     public static final String VERTEX_OSERIES_CLIENT_ID_PROPERTY = PROPERTY_PREFIX + "clientId";
     public static final String VERTEX_OSERIES_CLIENT_SECRET_PROPERTY = PROPERTY_PREFIX + "clientSecret";
+
     public static final String VERTEX_OSERIES_COMPANY_NAME_PROPERTY = PROPERTY_PREFIX + "companyName";
     public static final String VERTEX_OSERIES_COMPANY_DIVISION_PROPERTY = PROPERTY_PREFIX + "companyDivision";
-    public static final String VERTEX_OAUTH_TOKEN = "VERTEX_OAUTH_TOKEN";
 }
